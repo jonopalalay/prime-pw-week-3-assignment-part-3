64da6ac6ac22c77cafa01d0c6effaac71370f9e5
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-05 01:09:58 -0600

    second save point

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -30,10 +30,10 @@ supplyChanges.push(25);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
-for(let x=0; x < supplyChanges.length; x++){
-  console.log(supplyChanges[ x ])
+for(x in supplyChanges){
+  console.log(supplyChanges [x])
 }
-  if (x > 0){
+  if(x > 0){
     console.log('Added x parts')
   }
     else if (x = 0) {
@@ -41,16 +41,28 @@ for(let x=0; x < supplyChanges.length; x++){
     }
       else if (x < 0) {
         console.log('Removed x parts')
-      };
+      }
+
+
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+for ( part of supplyChanges ){
+  console.log ( 'for of loop', part );
+}
+
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
 
+let sum = 0;
+
+for (let i = 0); i < supplyChanges; i++){
+  sum += supplyChanges[i];
+}
+console.log (sum);
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
 //    There are 572 parts in total, and each box holds 7 parts.
