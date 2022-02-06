9fe23f69d1081436150a3cd22af6e962c5e49515
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-05 18:18:28 -0600

    completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,7 +10,7 @@ console.log(partsNeeded)
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = [ '3','5','-6','0','7','11'];
+let supplyChanges = [ 3, 5, -6, 0, 7, 11];
 console.log(supplyChanges)
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChanges is:');
@@ -30,26 +30,32 @@ supplyChanges.push(25);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
-for(x in supplyChanges){
-  console.log(supplyChanges [x])
-}
-  if(x > 0){
-    console.log('Added x parts')
+for(let x=0; x<supplyChanges.length; x++){
+  if(supplyChanges [x] > 0){
+    console.log(`Added ${supplyChanges [x]} parts`)
   }
-    else if (x = 0) {
+    else if (supplyChanges [x] === 0) {
       console.log('No Change')
     }
-      else if (x < 0) {
-        console.log('Removed x parts')
+  else {
+        console.log(`Removed ${supplyChanges [x]} parts`)
       }
-
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-for ( part of supplyChanges ){
-  console.log ( 'for of loop', part );
+for ( x of supplyChanges ){
+  if(x > 0){
+    console.log(`Added ${x} parts`)
+}
+  else if (x === 0) {
+    console.log('No Change')
+  }
+else {
+      console.log(`Removed ${x} parts`)
+    }
 }
 
 
@@ -59,9 +65,11 @@ console.log('8. Total supplies available is:');
 
 let sum = 0;
 
-for (let i = 0); i < supplyChanges; i++){
-  sum += supplyChanges[i];
+for (let i=0; i<supplyChanges.length; i++) {
+  sum+=supplyChanges[i];
 }
+
+
 console.log (sum);
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
@@ -70,3 +78,14 @@ console.log (sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let parts = 572;
+let boxes = [];
+
+while (parts > 6){
+    parts-=7;
+    boxes.push(boxes.length);
+}
+
+console.log(`There were ${boxes.length} boxes filled.`);
+console.log(`There are ${parts} parts left over.`);
